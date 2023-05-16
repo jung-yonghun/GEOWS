ecad509a82a7b25a2c5e8bb0c4c31b63038c5ab2
jung-yonghun <yhjung@esein.co.kr>
2023-05-16 16:06:21 +0900

    Create 11.sql

---
+++ 11.sql
@@ -0,0 +1,21 @@
+select * from CPS.[dbo].[IMAC100]
+where ImBlNo='YLC05283320'
+
+select * from CPS.[dbo].[IMAC100]
+where ImNapSaup='8748602266'
+
+select * from CPS.[dbo].[IMAC105]
+where ImKey='I2023032442119100644'
+
+select * from CPS.[dbo].[IMAC115]
+where ImKey='I2023032442119100644'
+
+
+select * from CPS.[dbo].[IMAC105]
+where ImKey in (
+select ImKey from CPS.[dbo].[IMAC100]
+where ImNapSaup='8748602266'
+and UseYn='Y'
+)
+and UseYn='Y'
+and AdjCd in ('C010','C078')
